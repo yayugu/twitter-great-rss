56dc35ea5eb6052a6c552a1af69fcbaeae13f06d
sanadan <jecy00@gmail.com>
2018-07-05 16:43:01 +0900

    Adjust to work locally.
    Not worked on Ruby 2.4.4/2.5.1. Use 2.3.7.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source 'https://rubygems.org'
 
-ruby '2.2.3'
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.8'
 
@@ -35,6 +33,7 @@ end
 
 group :development do
   gem 'sqlite3'
+  gem 'dotenv-rails'
 end
 
 # Use ActiveModel has_secure_password
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+Dotenv::Railtie.load
+
 module TwitterGreatRss
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
