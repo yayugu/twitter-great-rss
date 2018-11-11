116288d6ec9fd6badc06d60954ebd137bc21a386
sanadan <jecy00@gmail.com>
2018-11-11 15:22:27 +0900

    Add tweet_mode=extended

--- lib/markup_tweet.rb
+++ lib/markup_tweet.rb
@@ -1,7 +1,7 @@
 class MarkupTweet
   class << self
     def markup_tweet(tweet)
-      text = tweet['text']
+      text = tweet['full_text']
       entities = tweet['entities']
       extended_entities = tweet['extended_entities']
       text = MarkupTweet::markup_media(text, entities, extended_entities)
@@ -62,7 +62,7 @@ class MarkupTweet
     def markup_quote(text, tweet)
       return text unless tweet['quoted_status']
       quoted_status = tweet['quoted_status']
-      "#{text}<blockquote>#{MarkupTweet::markup_author(quoted_status['text'], quoted_status)}</blockquote>"
+      "#{text}<blockquote>#{MarkupTweet::markup_author(quoted_status['full_text'], quoted_status)}</blockquote>"
     end
 
     def markup_author(text, tweet)
